0eb77b10871fce11f57f703397dd26eb59b8a563
chrisfauries <45318711+chrisfauries@users.noreply.github.com>
2018-12-03 17:08:56 -0600

    created form elements on contactus.html; created simple form validation

--- Walsh Website Rebuild/scripts/main.js
+++ Walsh Website Rebuild/scripts/main.js
@@ -23,3 +23,53 @@ footerHTML.innerHTML = '<div id="footer"><div class="single-contact"><p>Director
 body.appendChild(footerHTML);
 
 
+/* Form Validation */
+
+
+var contactUsForm = document.forms.contactUsForm;
+
+var errorMessageFirstName = document.getElementById("error-message-1");
+
+var errorMessageLastName = document.getElementById("error-message-2");
+
+var errorMessageEmail = document.getElementById("error-message-3");
+
+contactUsForm.onsubmit = function(){
+    
+    if(contactUsForm.adultFirstName.value == ""){
+       
+       errorMessageFirstName.innerHTML = "field required";
+       return false;
+    }else{
+       errorMessageFirstName.innerHTML = "";
+       return true;
+       }
+    
+};
+
+
+contactUsForm.onsubmit = function(){
+    
+    if(contactUsForm.adultLastName.value == ""){
+       
+       errorMessageLastName.innerHTML = "field required";
+       return false;
+    }else{
+       errorMessageFirstName.innerHTML = "";
+       return true;
+       }
+    
+};
+
+contactUsForm.onsubmit = function(){
+    
+    if(contactUsForm.email.value == ""){
+       
+       errorMessageEmail.innerHTML = "field required";
+       return false;
+    }else{
+       errorMessageEmail.innerHTML = "";
+       return true;
+       }
+    
+};

1288e93791d8c4c1a9f2043e6482f4d4b73c91e5
chrisfauries <45318711+chrisfauries@users.noreply.github.com>
2019-02-10 17:39:42 -0600

    made CSS changes to the header; added a jquery image carousel to the index page

--- Walsh Website Rebuild/scripts/headerfooter.js
+++ Walsh Website Rebuild/scripts/headerfooter.js
@@ -1,24 +1,28 @@
-var mobileNavMenu = document.getElementById("nav"); 
+document.onload = function() {
+	
+	var mobileNavMenu = document.getElementById("nav"); 
 
-var mobileNavMenuButton = document.getElementById("mobile-menu-title");
+	var mobileNavMenuButton = document.getElementById("mobile-menu-title");
 
 
-mobileNavMenuButton.onclick = function(){
-    
-    if (mobileNavMenu.className == "show-mobile-menu"){
-        mobileNavMenu.className = "";
-        
-        
-    }else{
-        mobileNavMenu.className = "show-mobile-menu";
-    }
-    
-}
+	mobileNavMenuButton.onclick = function(){
 
-window.onclick = function(event) {
-    if (!event.target.matches("#mobile-menu-title")) {
+			if (mobileNavMenu.className == "show-mobile-menu"){
+					mobileNavMenu.className = "";
 
-    mobileNavMenu.className = "";
-    }
-    
+
+			}else{
+					mobileNavMenu.className = "show-mobile-menu";
+			}
+
+	}
+
+	window.onclick = function(event) {
+			if (!event.target.matches("#mobile-menu-title")) {
+
+			mobileNavMenu.className = "";
+			}
+
+	}
+
 }
--- Walsh Website Rebuild/scripts/main.js
+++ Walsh Website Rebuild/scripts/main.js
@@ -1,64 +1,88 @@
 // JavaScript for index.html (Walsh Band Website)
 
-/* Header Insertion - jQuery*/
+// Header Insertion - jQuery
 
 
-$("#headerloadin").load("header.html");
+$("#headerLoadIn").load("header.html");
 
 
-/* Header Insertion - jQuery*/
+//Footer Insertion - jQuery
 
 
-$("#footerloadin").load("footer.html");
+$("#footerLoadIn").load("footer.html");
 
 
-/* Form Validation - simple */
+// Picture Carousel
+
+$(".rslides").responsiveSlides({
+	auto: true,             // Boolean: Animate automatically, true or false
+  speed: 1000,            // Integer: Speed of the transition, in milliseconds
+  timeout: 5000,          // Integer: Time between slide transitions, in milliseconds
+  pager: false,           // Boolean: Show pager, true or false
+  nav: false,             // Boolean: Show navigation, true or false
+  random: false,          // Boolean: Randomize the order of the slides, true or false
+  pause: false,           // Boolean: Pause on hover, true or false
+  pauseControls: true,    // Boolean: Pause when hovering controls, true or false
+  prevText: "Previous",   // String: Text for the "previous" button
+  nextText: "Next",       // String: Text for the "next" button
+  maxwidth: "",           // Integer: Max-width of the slideshow, in pixels
+  navContainer: "",       // Selector: Where controls should be appended to, default is after the 'ul'
+  manualControls: "",     // Selector: Declare custom pager navigation
+  namespace: "rslides",   // String: Change the default namespace used
+  before: function(){},   // Function: Before callback
+  after: function(){}     // Function: After callback
+	
+	
+});
+
+// Form Validation - simple 
 
 
-var contactUsForm = document.forms.contactUsForm;
+//var contactUsForm = document.forms.contactUsForm;
+//
+//var errorMessageFirstName = document.getElementById("error-message-1");
+//
+//var errorMessageLastName = document.getElementById("error-message-2");
+//
+//var errorMessageEmail = document.getElementById("error-message-3");
+//
+//contactUsForm.onsubmit = function(){
+//
+//		if(contactUsForm.adultFirstName.value == ""){
+//
+//			 errorMessageFirstName.innerHTML = "field required";
+//			 return false;
+//		}else{
+//			 errorMessageFirstName.innerHTML = "";
+//			 return true;
+//			 }
+//
+//};
+//
+//
+//contactUsForm.onsubmit = function(){
+//
+//		if(contactUsForm.adultLastName.value == ""){
+//
+//			 errorMessageLastName.innerHTML = "field required";
+//			 return false;
+//		}else{
+//			 errorMessageFirstName.innerHTML = "";
+//			 return true;
+//			 }
+//
+//};
+//
+//contactUsForm.onsubmit = function(){
+//
+//		if(contactUsForm.email.value == ""){
+//
+//			 errorMessageEmail.innerHTML = "field required";
+//			 return false;
+//		}else{
+//			 errorMessageEmail.innerHTML = "";
+//			 return true;
+//			 }
+//
+//};
 
-var errorMessageFirstName = document.getElementById("error-message-1");
-
-var errorMessageLastName = document.getElementById("error-message-2");
-
-var errorMessageEmail = document.getElementById("error-message-3");
-
-contactUsForm.onsubmit = function(){
-    
-    if(contactUsForm.adultFirstName.value == ""){
-       
-       errorMessageFirstName.innerHTML = "field required";
-       return false;
-    }else{
-       errorMessageFirstName.innerHTML = "";
-       return true;
-       }
-    
-};
-
-
-contactUsForm.onsubmit = function(){
-    
-    if(contactUsForm.adultLastName.value == ""){
-       
-       errorMessageLastName.innerHTML = "field required";
-       return false;
-    }else{
-       errorMessageFirstName.innerHTML = "";
-       return true;
-       }
-    
-};
-
-contactUsForm.onsubmit = function(){
-    
-    if(contactUsForm.email.value == ""){
-       
-       errorMessageEmail.innerHTML = "field required";
-       return false;
-    }else{
-       errorMessageEmail.innerHTML = "";
-       return true;
-       }
-    
-};
